afb74db7c6c66aa536c345a232a90e3c65f006a8
Abhishek Chouhan <chouhanabhi5@gmail.com>
2020-04-23 22:40:37 +0530

    debugger, unittest added

---
+++ pdb_debugger_commands.py
@@ -0,0 +1,22 @@
+### To invoke debugger (pdb.py) in the interpretter:
+# python -m pdb myscript.py
+### Then use debugger commands
+
+### Or can insert:
+# import pdb
+# pdb.set_trace()
+### at the location you want to break into debugger.
+
+
+### Commands:
+# h : help
+# n           : execute next line
+# c           : complete execution
+# l           : list 3 lines before and after current line
+# s           : step into function call
+# b           : show list of all break points
+# b[int]      : set break point at line number (eg. b10)
+# b [func]    : break at function name
+# cl          : clear all break point
+# cl[int]     : clear break point at line number
+# p           : print
---
+++ unit_test.py
@@ -0,0 +1,79 @@
+import unittest
+
+
+class TestMyClass(unittest.TestCase):
+    '''
+        Name of all test cases methods must start with 'test_',
+        if not then the test won't run.
+
+        flow:
+            setupClass
+
+            setUp
+            test_method1
+            tearDown
+
+            setUp
+            test_method1
+            tearDown
+
+            .
+            .
+
+            tearDownClass
+    '''
+
+    @classmethod
+    def setUpClass(cls):
+        '''
+            Class method. Method name must be 'setUpClass'.
+            Called once, at the start of testing.
+        '''
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        '''
+            Class method. Method name must be 'tearDownClass'.
+            Called once, at the end of testing.
+        '''
+        pass
+
+    def setUp(self):
+        '''
+            Method name must be 'setUp'. Called before each test cases,
+            mostly initialize instance variable which are
+            common for all test cases.
+        '''
+        pass
+        # ex: self.instance = MyClass('var1', 'var2')
+
+    def tearDown(self):
+        '''
+            Method name must be 'tearDown'. Called after each test cases,
+            Used as destructor for instances created in setUp method.
+        '''
+        pass
+
+    def test_method1(self):
+        '''
+            Name must start with 'test_'
+        '''
+        # Assert equal or not
+        self.assertEqual('foo'.upper(), 'FOO')
+
+        # Assert True/False
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+        # Check if proper exception is raised or not
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+# Unit test can bb run using command line interface
+# for that comment the below code and run by 'python -m unittest this_file_name.py'
+# if __name__ == '__main__':
+#     unittest.main()
